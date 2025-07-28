82182b46424868edf9e559022535da4c18433c35
Divyang Solanki <divyangsolanki2004@gmail.com>
2025-07-28 10:53:41 +0530

    Update student_teacher.py

--- student_teacher.py
+++ student_teacher.py
@@ -13,16 +13,24 @@ class Student:
 
 
 # Create student objects
-student1 = Student("Aman")
-student2 = Student("Divya")
+student1 = Student("Divyang")
+student2 = Student("Mann")
+student3 = Student("Heli")
 
 # Add subjects and marks
 student1.add_subject("Math", 85)
 student1.add_subject("Science", 92)
+student1.add_subject("English", 92)
 
 student2.add_subject("Math", 78)
 student2.add_subject("English", 88)
+student2.add_subject("Science", 88)
+
+student3.add_subject("Math", 75)
+student3.add_subject("English", 68)
+student3.add_subject("Science", 98)
 
 # Show reports
 student1.show_report()
 student2.show_report()
+student3.show_report()
